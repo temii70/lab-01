f6b42902651380c4cf9b8056f5fe3364600a7145
Temii70 <otun@ualberta.ca>
2026-01-08 00:32:30 -0700

    step 6

---
+++ PetShop/app/src/main/java/com/example/petshop/Pet.java
@@ -0,0 +1,41 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Pet {
+    private String name;
+    private Date birthDate;
+
+    public Pet(String name) {
+        this.name = name; //java thinks both names refer to parameters not the fields
+        //so we need this to differientate them
+        this.birthDate = new Date();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    //constructor has NO return types
+    public Pet(Date birthDate, String name) {
+        this.birthDate = birthDate;
+        this.name = name; //set objects name to the value passed in
+
+    }
+
+}
+
+
+
